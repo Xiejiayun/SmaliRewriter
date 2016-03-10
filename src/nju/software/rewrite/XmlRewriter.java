b561e06e76e874113a6f7710ad766e4c44e43acc
xie <furnace09@gmail.com>
2016-03-10 16:47:22 +0800

    refactor the project to add more rules

--- src/nju/software/rewrite/XmlRewriter.java
+++ src/nju/software/rewrite/XmlRewriter.java
@@ -14,6 +14,7 @@ public class XmlRewriter extends AbstractRewriter {
     @Override
     public void search(String smaliFile, String ruleFile, List<String> messageList) {
 
+
     }
 
     @Override
